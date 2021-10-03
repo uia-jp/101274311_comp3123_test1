780333de8c33c6e7a3b9fdce94990eff5fbedbed
Yukina Ishiguro <68873109+uia-jp@users.noreply.github.com>
2021-10-03 19:59:20 -0400

    q1

--- question-1/ lowerCaseWords.js
+++ question-1/ lowerCaseWords.js
@@ -1,1 +1,28 @@
- 
+
+
+const lowerCaseWords = (mixedArray) => {
+
+    return new Promise((resolve, reject) => {
+
+        if(mixedArray.length >= 0 && Array.isArray(mixedArray)) {
+            const filteredArrayHavingOnlyStr = mixedArray.filter((eachElOfArr) => typeof eachElOfArr === "string");
+            const mappedArrayOfLowerCaseStrings = filteredArrayHavingOnlyStr.map((eachItemOfFilterArr) => eachItemOfFilterArr.toLowerCase());
+            
+            resolve(mappedArrayOfLowerCaseStrings);
+            
+        }else{
+            reject("Invalid array");
+        }
+    });
+};
+  
+//input
+const mixedArray = ["PIZZA", 10, true, 25, false, "Wings"];
+
+lowerCaseWords(mixedArray)
+    .then((value) =>
+      console.log(value)
+    )
+    .catch((error) =>
+      console.log(error)
+    );
